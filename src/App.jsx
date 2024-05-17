d5fff635a70793d7090bafdd4cc289e94dfc24bc
Uxdruh <uxdruh@gmail.com>
2024-05-17 14:49:05 +0800

    Create 3 components, and initialize them

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
+import Footer from "./components/Footer"
+import Main from "./components/Main"
+import SideBar from "./components/SideBar"
+
 function App() {
 
   return (
     <>
-
+      <SideBar />
+      <Main />
+      <Footer />
     </>
   )
 }
